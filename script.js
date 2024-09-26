f1dcb1ba8d5666ea1b1e791b17fd899e58c930a7
João Henrique <joaohenriquerbueno@gmail.com>
2024-09-26 12:51:28 -0300

    substituido classes por dataset

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  if (tabMenu.length && tabMenu.length) {
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
     function activeTab(index) {
-      tabContent.forEach((content) => {
-        content.classList.remove('ativa');
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
       });
-      tabContent[index].classList.add('ativa');
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add('ativo', direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -17,16 +20,21 @@ function initTabNav() {
     });
   }
 }
+initTabNav();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-acoordion dt');
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt',
+  );
+  const activeClass = 'ativo';
+
   if (accordionList.length) {
-    accordionList[0].classList.add('ativa');
-    accordionList[0].nextElementSibling.classList.add('ativa');
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
     function activeAccordion() {
-      this.classList.toggle('ativa');
-      this.nextElementSibling.classList.toggle('ativa');
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
     }
 
     accordionList.forEach((item) => {
@@ -34,48 +42,53 @@ function initAccordion() {
     });
   }
 }
+initAccordion();
 
-function initScrolSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]',
+  );
 
-  if (linksInternos.length) {
-    function scrollTosection(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    }
-
-    linksInternos.forEach((link) => {
-      link.addEventListener('click', scrollTosection);
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
     });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
   }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
+initScrollSuave();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
   if (sections.length) {
-    const windowMetade = innerHeight * 0.3;
+    const windowMetade = window.innerHeight * 0.6;
+
     function animaScroll() {
       sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = sectionTop - windowMetade < 0;
-
-        if (isSectionVisible) {
-          section.classList.add('ativa');
-        }
+        if (isSectionVisible) section.classList.add('ativo');
+        else section.classList.remove('ativo');
       });
     }
+
     animaScroll();
 
     window.addEventListener('scroll', animaScroll);
   }
 }
-initTabNav();
-initAccordion();
-initScrolSuave();
 initAnimacaoScroll();
