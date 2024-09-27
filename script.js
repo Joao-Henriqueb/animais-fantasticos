224a33fa97e02b5b8c6705ef60b873ed1cbec0d7
João Henrique <joaohenriquerbueno@gmail.com>
2024-09-27 13:13:40 -0300

    adicionado script do modal

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initAccordion from './modules/accordian.js';
 import initTabNav from './modules/tabNav.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initScrollSuave from './modules/scroll-suave.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
