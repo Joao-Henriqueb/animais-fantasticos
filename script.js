d5df1080804d5902261e6237cbd5bc5243579bf9
João Henrique <joaohenriquerbueno@gmail.com>
2024-10-01 10:47:19 -0300

    adicionado tooltip e dropdowmenu

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ import initTabNav from './modules/tabNav.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropdownMenu();
