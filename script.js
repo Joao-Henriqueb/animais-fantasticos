1338510be0d5c85aeb67e0c9fc599e82c967ad77
João Henrique <joaohenriquerbueno@gmail.com>
2024-10-03 10:22:44 -0300

    adicionado menu mobile

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
